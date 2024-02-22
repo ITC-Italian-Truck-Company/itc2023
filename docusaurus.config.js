5104f0ac18817e8d6cd944d2caf45c70540a8a4d
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-22 11:23:04 +0100

    chore: inserito Yana_PewYT nella lista streamer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,6 +99,10 @@ const config = {
               {
                 label: 'italiantruckcompany2023',
                 href: 'https://www.twitch.tv/italiantruckcompany2023',
+              },
+              {
+                label: 'Yana_PewYT',
+                href: 'https://www.twitch.tv/yana_pewyt',
               }
             ],
           },
