8686d12e6b0834e563baefcc171e464ff0ec8f7c
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-05-06 12:08:41 +0200

    feat: build-deploy-and-preview.yml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   url: 'https://italiantruckcompany.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/itc2023/',
+  baseUrl: process.env.GITHUB_ACTIONS ? `${process.env.BASE_URL}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
